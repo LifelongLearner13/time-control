dfcc14581a4fc21b8a56a17716986933217e1bef
LifelongLearner13 <sierragregg@gmail.com>
2017-01-05 17:49:43 -0600

    added clarifying comments

--- js/background.js
+++ js/background.js
@@ -1,22 +1,35 @@
-// When the extension is installed or upgraded ...
+/*----- Background Script -----*/
+// Runs ccontinuously in the background listening and
+// reacting to browser events.
+
+// Fires when the extension is installed or updated
 chrome.runtime.onInstalled.addListener(function() {
   // Replace all rules ...
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
+    // Add a new rule to active extension when page matches the specified
+    // criteria, i.e. host contains "google.com", the path contains search,
+    // and the css contains the class ".gsfi"
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {
-              urlContains: 'google'
-            }
-          })],
-        // And shows the extension's page action.
+              hostSuffix: 'google.com',
+              pathContains: 'search',
+            },
+              css: [".gsfi"]
+            })],
+        // Activate/show extension
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }
     ]);
   });
 });
 
+// Listens for navigation events. If a url matches the filters listed
+// below, the listener intercepts the request before it is loaded
+// in the browser. If a date range has been saved in the browser's
+// storage, the appropriate parameter is added to the end of the url
+// and the page is reloaded.
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
   console.log(details)
   chrome.storage.local.get('dateConfig', function(items) {
@@ -38,6 +51,3 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
     }
   ]
 });
-// There is a lot of variation in how google's urls are organized across services and countries
-// The above filters should limit the event to searches made through the American version
-// of google.com.
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,9 @@
+/*----- Popup Script -----*/
+// Controls the interactivity of the extensions
+// popup. This script is executed in a sandbox,
+// so DOM modifications made here will not effect
+// the overall page.
+
 function runSubmit(event) {
   event.preventDefault();
   console.log(event);
@@ -7,12 +13,16 @@ function runSubmit(event) {
   chrome.storage.local.set({
     dateConfig: value
   }, function () {
+
+    // Get the tabID of the open tab behind the popup.
     var query = { active:true,windowType:"normal", currentWindow: true };
     chrome.tabs.query(query, function(tabs) {
       var tabID = tabs[0].id;
       console.log(tabID)
       var oldURL = window.location.href;
       var newURL = oldURL + '&' + value;
+
+      // Reload the page with the new parameter
       chrome.tabs.update(tabID, {
           url: newURL,
       });
